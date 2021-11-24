f1cabbd73d4da35a7c70d5464ebe1cf1f8af8d87
Ubuntu <ubuntu@ip-172-31-90-22.ec2.internal>
2021-11-24 20:54:12 +0000

    edited Energy.swift

--- Energy.swift
+++ Energy.swift
@@ -1,16 +1,16 @@
 /*
-* The logs program, that calculates the amount of logs capable of being in a truck
-* Based on length given.
+* The energy program, that calculates the potential energy of
+* an object given mass.
 *
 * @author  Matthew Sanii
 * @version 1.0
 * @since   2021-22-11
 */
 
-let light : Double = 8.988004
+let light: Double = 8.988004
 print("Enter mass of object: ")
 let input = readLine()
 if let mass = Double(input!) {
 let energy = light * mass
-print("Potential energy is " , energy , "E16J.")
-} 
+print("Potential energy is ", energy, "E16J.")
+}
